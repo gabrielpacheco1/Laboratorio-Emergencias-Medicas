1085ecc3df3dc1849fa33f9aa845b10de1587b13
Gabriel Pacheco <gabrielp.lafray@gmail.com>
2018-10-24 12:30:03 -0300

    Inicio

---
+++ src/emergencias_medicas/Emergencias_Medicas.java
@@ -0,0 +1,16 @@
+
+package emergencias_medicas;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Emergencias_Medicas {
+
+    public static void main(String[] args) {
+        
+        System.out.println("\n");
+        
+    }
+    
+}
