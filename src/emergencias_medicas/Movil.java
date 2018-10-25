47a0685ddc1a16fb81ae9a1c31167655745b78a1
Gabriel Pacheco <gabrielp.lafray@gmail.com>
2018-10-25 15:13:29 -0300

    Clases añadidas

---
+++ src/emergencias_medicas/Movil.java
@@ -0,0 +1,65 @@
+
+package emergencias_medicas;
+
+class Movil {
+    private String marca;
+    private String modelo;
+    private int año;
+    private String patente;
+
+    /**
+     * @return the marca
+     */
+    public String getMarca() {
+        return marca;
+    }
+
+    /**
+     * @param marca the marca to set
+     */
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    /**
+     * @return the modelo
+     */
+    public String getModelo() {
+        return modelo;
+    }
+
+    /**
+     * @param modelo the modelo to set
+     */
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    /**
+     * @return the año
+     */
+    public int getAño() {
+        return año;
+    }
+
+    /**
+     * @param año the año to set
+     */
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    /**
+     * @return the patente
+     */
+    public String getPatente() {
+        return patente;
+    }
+
+    /**
+     * @param patente the patente to set
+     */
+    public void setPatente(String patente) {
+        this.patente = patente;
+    }
+}
---
+++ src/emergencias_medicas/Resultado.java
@@ -0,0 +1,37 @@
+
+package emergencias_medicas;
+
+public class Resultado {
+    
+    private String tipo;
+    private String diagnostico;
+
+    /**
+     * @return the tipo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @return the diagnostico
+     */
+    public String getDiagnostico() {
+        return diagnostico;
+    }
+
+    /**
+     * @param diagnostico the diagnostico to set
+     */
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+    
+}
